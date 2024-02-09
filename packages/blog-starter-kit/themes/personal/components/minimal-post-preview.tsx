81c3102096cada5bcb7f87edd320c98c899fcfae
rajatkapoor <rajat@hashnode.com>
2024-02-09 20:31:46 +0530

    Added commented code

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -10,9 +10,15 @@ type Props = {
 	author: Author;
 	slug: string;
 	commentCount: number;
+	// tags: PostFragment['tags'];
 };
 
-export const MinimalPostPreview = ({ title, date, slug, commentCount }: Props) => {
+export const MinimalPostPreview = ({
+	title,
+	date,
+	slug,
+	commentCount, // tags
+}: Props) => {
 	const postURL = `/${slug}`;
 
 	return (
@@ -20,6 +26,16 @@ export const MinimalPostPreview = ({ title, date, slug, commentCount }: Props) =
 			<h2 className="text-lg leading-tight tracking-tight text-black dark:text-white">
 				<Link href={postURL}>{title}</Link>
 			</h2>
+			{/* <p className="flex flex-row gap-2">
+				{tags?.map((tag) => (
+						<span
+							key={tag.slug}
+							className="rounded-lg border border-orange-500 px-2 text-sm text-neutral-600 dark:text-neutral-400"
+						>
+							{tag.name}
+						</span>
+				))}
+			</p> */}
 			<p className="flex flex-row items-center gap-2">
 				<Link href={postURL} className="text-sm text-neutral-600 dark:text-neutral-400">
 					<DateFormatter dateString={date} />
